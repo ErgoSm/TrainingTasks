d08b6d2ed577492e4a974fbec0a22273bd3b0bb3
Егор Смирнов <egorsmirn@outlook.com>
2022-06-24 11:21:21 +0400

    Fix T3 LeaderboardLL

--- TrainingTasks/Task3/CustomLinkedList.cs
+++ TrainingTasks/Task3/CustomLinkedList.cs
@@ -8,11 +8,14 @@ namespace TrainingTasks.Task3
 {
     internal sealed class CustomLinkedList<T>
     {
-
+        private List<CustomItem<T>> _items;
 
         public void AddFirst(T item)
         {
-
+            /*if(_items.Count == 0)
+                _items.Insert(0, new CustomItem<T>(item));
+            else
+                _items.Insert(0, new CustomItem<T>(item), );*/
         }
 
         public void AddLast(T item)
@@ -29,5 +32,31 @@ namespace TrainingTasks.Task3
         {
 
         }
+    }
+
+    internal sealed class CustomItem<T>
+    {
+        public readonly CustomItem<T> Next;
+        public readonly CustomItem<T> Prev;
+
+        private T _item { get; set; }
+
+
+        internal CustomItem(T item)
+        {
+            _item = item;
+        }
+
+        internal CustomItem(T item, CustomItem<T> next, CustomItem<T> prev = null) : this(item)
+        {
+            Next = next;
+            Prev = prev;
+        }
+
+        internal CustomItem(T item, CustomItem<T> prev) : this(item)
+        {
+            Prev = prev;
+        }
+
     }
 }
--- TrainingTasks/Task3/LeaderboardLL.cs
+++ TrainingTasks/Task3/LeaderboardLL.cs
@@ -2,7 +2,7 @@
 {
     internal sealed class LeaderboardLL : LeaderboardClass
     {
-        private Dictionary<string, Entity> _entityLinks = new Dictionary<string, Entity>();
+        private Dictionary<string, LinkedListNode<Entity>> _entityLinks = new Dictionary<string, LinkedListNode<Entity>>();
         private LinkedList<Entity> _entities = new LinkedList<Entity>();
         private LinkedListNode<Entity> _entityNode;
 
@@ -18,36 +18,37 @@
                 var entity = _entityLinks[id];
 
                 _entities.Remove(entity);
+                entity.Value.Score = score;
 
-                entity.Score = score;
-                _entityNode = GetNearesSmallerEntity(entity);
-
-                if (_entityNode == null)
-                    _entities.AddLast(entity);
-                else
-                    _entities.AddBefore(_entityNode, entity);
+                Insert(_entities.First, entity.Value, score);
             }
             else
             {
                 var newEntity = new Entity { Id = id, Score = score };
-                _entityNode = GetNearesSmallerEntity(newEntity);
-
-                if (_entityNode == null)
-                    _entities.AddLast(newEntity);
-                else
-                    _entities.AddBefore(_entityNode, newEntity);
-
-                _entityLinks.Add(id, newEntity);
+                _entityLinks.Add(id, Insert(_entities.First, newEntity, score));
             }
         }
 
-        private LinkedListNode<Entity> GetNearesSmallerEntity(Entity newEntity)
+        private LinkedListNode<Entity> Insert(LinkedListNode<Entity> startNode, Entity entity, int score)
         {
-            foreach (var entity in _entities)
-                if (entity.Score < newEntity.Score)
-                    return _entities.Find(entity);
 
-            return null;
+            _entityNode = GetNearestSmallerEntity(startNode, score);
+
+            if (_entityNode == null)
+                _entityNode = _entities.AddLast(entity);
+            else
+                _entityNode = _entities.AddBefore(_entityNode, entity);
+
+            return _entityNode;
+        }
+
+        private LinkedListNode<Entity> GetNearestSmallerEntity(LinkedListNode<Entity> startEntity, int score)
+        {
+            while (startEntity != null && startEntity.Value.Score > score)
+                startEntity = startEntity.Next;
+                
+
+            return startEntity;
         }
     }
 }
