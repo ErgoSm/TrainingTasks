4d8c48ebaf8306de19743f6f83e8fce42e6a3d61
Егор Смирнов <egorsmirn@outlook.com>
2022-06-27 13:17:44 +0400

    Add contest tasks

---
+++ TrainingTasks/Contest256/TaskD.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+var number = Convert.ToInt32(Console.ReadLine());
+var regex = new Regex(@"(\-|\d|\w)+");
+for (int i = 0; i < number; i++)
+{
+    var attempts = Convert.ToInt32(Console.ReadLine());
+    var dictionary = new Dictionary<string, string>(200000);
+    string result = "YES";
+
+    for (int j = 0; j < attempts; j++)
+    {
+        result = "NO";
+        var str = Console.ReadLine().ToUpper();
+
+        if (str.Length >= 2 && str.Length <= 24 && str[0] != 45 && dictionary.TryAdd(str, str))
+        {
+            if (regex.Match(str)?.Value == str)
+                result = "YES";
+
+            //if (dictionary[str].Overlaps(nonValidSet))
+            //    result = "NO";
+        }
+        else
+            result = "NO";
+
+        Console.WriteLine(result);
+    }
+
+    Console.WriteLine();
+}
---
+++ TrainingTasks/Task4/Map.cs
@@ -0,0 +1,41 @@
+﻿namespace TrainingTasks.Task4
+{
+    internal readonly record struct Flat(int X, int Y, int Price);
+
+    internal sealed class Map
+    {
+        private Dictionary<int, Flat> _flats = new Dictionary<int, Flat>();
+
+        internal Map()
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < 20000; i++)
+                _flats.Add(i, new Flat(rnd.Next(1000), rnd.Next(1000), rnd.Next(1000000, 40000000)));
+        }
+
+        internal IEnumerable<Flat> GetView(int x, int y, byte zoomLevel, int targetPrice, int maxObjects)
+        {
+            var relevantFlats = new List<Flat>();
+
+
+
+            return relevantFlats;
+        }
+
+        internal void UpdatePrice(int x, int y, int newPrice)
+        {
+
+        }
+
+        private Flat FindMostAppropriate()
+        {
+            return new Flat();
+        }
+    }
+
+    internal class Square
+    {
+        private int _x;
+    }
+}
